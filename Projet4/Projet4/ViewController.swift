40c690abdf28486e510aab68cf4467f5f3aaaedd
Lio <ekoue7@hotmail.fr>
2023-03-09 08:03:03 +0100

    Editing some contrains with the code

--- Projet4/Projet4/ViewController.swift
+++ Projet4/Projet4/ViewController.swift
@@ -4,16 +4,24 @@
 //
 //  Created by Lion on 02/03/2023.
 //
-
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createView()
     }
+    @IBOutlet weak var currentView: UIView!
 
-
+    func createView()
+    {
+        let rect = CGRect(x: 62, y: 220, width: 300, height: 300)
+        let layout = UIView(frame: rect)
+        layout.translatesAutoresizingMaskIntoConstraints = false
+        layout.backgroundColor = .blue
+        currentView.addSubview(layout)
+        let constrain = currentView.constraints
+    }
 }
-
---
+++ Projet4/Projet4/customUiview.swift
@@ -0,0 +1,22 @@
+//
+//  customUiview.swift
+//  Projet4
+//
+//  Created by Lion on 08/03/2023.
+//
+
+import UIKit
+
+class customUiview: UIView {
+
+    func ontap() -> (){
+        
+    }
+    
+    enum layout
+    {
+        case layout1,layout2,layout3
+    }
+
+    
+}
